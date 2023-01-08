88b0d089fd59db4eaced16ba0601a8bb49fec369
YPZ <YPZ@mail.com>
2023-01-08 09:28:57 +0800

    初始化项目

---
+++ src/main/java/com/ypz/ServicePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ypz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ypz/ServicePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ypz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
