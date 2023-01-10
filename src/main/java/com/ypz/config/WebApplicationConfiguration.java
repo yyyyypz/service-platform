bf0f90f6a15c4699a9ff9ab47629e3bbbada58c4
YPZ <YPZ@mail.com>
2023-01-10 17:06:12 +0800

    Revert "商品大类功能实现"
    This reverts commit eb754b73e169ffdd55dfdc38a7b0cc7c33cc2acb.

--- src/main/java/com/ypz/config/WebApplicationConfiguration.java
+++ src/main/java/com/ypz/config/WebApplicationConfiguration.java
@@ -9,9 +9,6 @@ public class WebApplicationConfiguration implements WebMvcConfigurer {
     // 静态资源映射
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // 轮播图图片路径映射
         registry.addResourceHandler("/images/swiper/**").addResourceLocations("file:D:\\service-platform-resources\\swiperImages\\");
-        // 商品大类图片路径映射
-        registry.addResourceHandler("/images/bigType/**").addResourceLocations("file:D:\\service-platform-resources\\bigTypeImages\\");
     }
 }
